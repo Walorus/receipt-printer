3b13f53d9e874e1210f3f8de7a883190d2892238
walorus <pvass153@gmail.com>
2016-04-23 05:43:36 +0000

    Added line indentation to addons and comments might remove later. Also added some code comments

--- receiptPrint.py
+++ receiptPrint.py
@@ -9,7 +9,7 @@ from urllib2 import urlopen
 from urllib2 import HTTPError
 from json import loads
 
-def main(orderDictionary):
+def main(orderDictionary): #almost all of the following is just formatting
 	printer = Adafruit_Thermal("/dev/ttyAMA0", 19200, timeout=5)
 	dict = orderDictionary
 	printer.setLineHeightSmall()
@@ -26,12 +26,7 @@ def main(orderDictionary):
 	for item in dict['items']:
 		printer.setSize("M")
 		printer.justify("L")
-		finalLine=item['name']
-		words = finalLine.split()
-		for i in range(0,len(words)):
-			if len(words[i])>24:
-				words[i]=words[i][:24]
-		finalLine = breakIntoLines(' '.join(words),printer)
+		finalLine = breakIntoLines(item['name'],False,printer)
 		printer.printNoLine(finalLine)
 		for i in range(0,lineLength-len(finalLine)):
                                 printer.printNoLine(".")
@@ -40,30 +35,22 @@ def main(orderDictionary):
 		printer.setSize("S")
 		printer.setLineHeightSmall()
 		for addon in item['addons']:
-			words2 = addon['name'].split()
-			for i in range(0,len(words2)):
-                       		if len(words2[i])>24:
-                                	words2[i]=words2[i][:24]
-			finalAddonLine = breakIntoLines(' '.join(words2),printer)
-                        printer.printNoLine(finalAddonLine)
-			for i in range(0,lineLength-len(finalAddonLine)):
+			finalAddonLine = breakIntoLines(addon['name'],True,printer)
+                        printer.printNoLine("  "+finalAddonLine)
+			for i in range(0,lineLength-len(finalAddonLine)-2):
                        		printer.printNoLine(".")
 			printer.printNoLine(" $")
 			printer.println('{0:.2f}'.format(addon['price']))
 			printer.println()
 		
 		if item['comments']:
-			printer.printNoLine("(")
-			words3 = item['comments'].split()
-                        for i in range(0,len(words3)):
-                                if len(words3[i])>24:
-                                        words3[i]=words3[i][:24]
-                        finalCommentLine = breakIntoLines(' '.join(words3),printer)
+			printer.printNoLine("  (")
+                        finalCommentLine = breakIntoLines(item['comments'],True,printer)
                         printer.printNoLine(finalCommentLine)
 			printer.println(")")
 
 		else:
-			printer.println("(No comments)")
+			printer.println("  (No comments)")
 	printer.setSize("M")
 	printer.println("Delivery Fee............ $5.00")
 	printer.println("______________________________")
@@ -74,27 +61,37 @@ def main(orderDictionary):
 		printer.printNoLine(".")
 	printer.printNoLine(" $")
 	printer.println('{0:.2f}'.format(dict['cost']+dict['driver']))
-	printer.setLineHeight(24)
-	printer.feed(3)
+	printer.setLineHeight(32)
+	printer.feed(8)
 	
 	#printer.setDefault() # Restore printer to defaults
 
-def breakIntoLines(currentLine,printer):
-	lastLine = currentLine
-	if len(currentLine)>=lineLength:	
+def breakIntoLines(currentLine,addSpaces,printer): #breaks lines greater than the lineLength set at top into properly sized lines
+	#If any words are > lineLength makes them less than lineLength
+	tempLineLength = lineLength
+	if addSpaces:
+		tempLineLength-=2
+	words = currentLine.split()
+	for i in range(0,len(words)):
+		if len(words[i])>tempLineLength:
+			words[i]=words[i][:tempLineLength]
+	lastLine = ' '.join(words)
+	if len(currentLine)>=tempLineLength:	#if the whole line is greater, removes words until it isn't
 		firstLine = ' '.join(currentLine.split()[:len(currentLine.split())-1])
 		arrayLength = len(currentLine.split())
 		wordsRemoved = 1
-		while(len(firstLine)>lineLength+5):
+		while(len(firstLine)>tempLineLength+5):
 			firstLine = ' '.join(firstLine.split()[:len(firstLine.split())-1])
 			wordsRemoved+=1
 		lastLine=' '.join(currentLine.split()[arrayLength-wordsRemoved:])
+		if addSpaces:
+			printer.printNoLine("  ")
 		printer.println(firstLine)
-		if len(lastLine)>lineLength :
-			lastLine = breakIntoLines(lastLine,printer)
+		if len(lastLine)>tempLineLength :
+			lastLine = breakIntoLines(lastLine,addSpaces,printer) #if the remaining removed words are still greater, does it again recursively
 	return lastLine
 
-def get_one_order(where=WHERE):
+def get_one_order(where=WHERE): #Aaron's method to get the order dictionary object, returns false if no objects available
     try:
         fd = urlopen(URL.format(where))
         text = fd.read().decode()
@@ -113,7 +110,7 @@ if __name__  == "__main__":
 		if orderReturn:
 			main(orderReturn)
 		else:
-			print("Sorry no orders")
+			print("No open orders currently") #Only seen in console, exists to show that it is running
 			time.sleep(25)
 		time.sleep(5)
 #	order = {'number':101,'cost':25,'tax':1.02,'driver':5.00,'total':30,'items':[{'name':"Burger",'price':10.50,'comments':"No tomato",
